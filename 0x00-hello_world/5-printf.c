df8703efee2a3a5a8150f6d6836f35c681adba12
Richard Gwati <haggio0844@gmail.com>
2023-02-16 10:21:57 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,6 @@
 /* main is the function body */
 int main(void) {
 /* function prinf prints a message */
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
