160c40bc2377ce429cc61781238d35e9588f110c
Richard Gwati <haggio0844@gmail.com>
2023-02-16 10:10:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
+//function puts prints a message and appends a new line
 int main(void) {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
