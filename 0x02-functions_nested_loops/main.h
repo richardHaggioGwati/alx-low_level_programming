9029e0d47426bffb32fcfe6d2d0d9b3a497c58bc
Richard Gwati <haggio0844@gmail.com>
2023-02-21 09:30:02 -0800

    code

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,18 @@ void print_alphabet(void) {
     }
     putchar('\n');
 }
+
+
+void print_alphabet_x10(void) {
+    char c;
+    int i;
+
+    for (i = 0; i < 10; i++) {
+        c = 'a';
+        while (c <= 'z') {
+            _putchar(c);
+            c++;
+        }
+        _putchar('\n');
+    }
+}
