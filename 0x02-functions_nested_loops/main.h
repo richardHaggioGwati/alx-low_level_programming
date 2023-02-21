64b570b28bbbcf067f3f17d26a944845ee11be43
Richard Gwati <haggio0844@gmail.com>
2023-02-21 09:12:03 -0800

    code

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,12 +3,11 @@
  */
 #include <stdio.h>
 
-
 void print_alphabet(void) {
     char c = 'a';
     while (c <= 'z') {
-        _putchar(c);
+        putchar(c);
         c++;
     }
-    _putchar('\n');
+    putchar('\n');
 }
