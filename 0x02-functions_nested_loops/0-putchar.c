242fb9ab11226fd70bdbff09c6b79db69c3bea05
Richard Gwati <haggio0844@gmail.com>
2023-02-21 08:12:35 -0800

    funner

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/**
+ * Prints _putchar, followed by a new line.
+ */
+#include <stdio.h>
+
+/**
+ * Function main will print out the desired result using printf
+ */
+int main(void) {
+printf("_putchar\n");
+/* Returns zero for a successful operation */
+return (0);
+}
