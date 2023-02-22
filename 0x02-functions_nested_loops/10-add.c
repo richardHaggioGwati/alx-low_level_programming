2a8999bd922c7361626e1dd1a634981a3e295b55
Richard Gwati <haggiog0844@gmail.com>
2023-02-22 17:56:41 +0200

    task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - add's 2 intergers
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new line.
  * return: the result of the operation
  */
 void print_to_98(int n)
