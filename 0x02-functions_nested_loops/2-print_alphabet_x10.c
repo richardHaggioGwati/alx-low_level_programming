547b8ce0659433b6dc0fc5f2c8648bcb64faf694
Richard Gwati <haggiog0844@gmail.com>
2023-02-22 14:20:51 +0200

    full name

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - prints all the alphabets  10 times in lowercase.
+ * print_alphabet_x10 - prints all the alphabets  10 times in lowercase.
  * The function is using a combination of for and while
  * loops to print the alphabets
  * function will return: On success 1.
