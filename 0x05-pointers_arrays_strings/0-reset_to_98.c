0b59b5fc21e3e1eba3e2c3df5831b2541127cf47
Richard Gwati <haggiog0844@gmail.com>
2023-02-27 14:08:19 +0200

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_remaining_days - takes int as parameter 
+* and updates the value it points to to 98
+* @n: n is our pointer parameter number
+* Return: void
+*/
+
+void reset_to_98(int *n)
+{
+int *p = &n;
+
+return (*p = 98);
+}
