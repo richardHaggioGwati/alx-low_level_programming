f4cb1d4dfd29f3633d3918164f416415a0ed8ade
Richard Gwati <haggiog0844@gmail.com>
2023-02-27 14:50:48 +0200

    betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-* print_remaining_days - takes int as parameter 
-* and updates the value it points to to 98
+* print_remaining_days - takes int as parameter and updates the value it points to to 98
 * @n: n is our pointer parameter number
 * Return: void
 */
