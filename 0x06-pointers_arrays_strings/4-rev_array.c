8096da9c26a7931f5c207f7dd572e9c373397966
Richard Gwati <haggiog0844@gmail.com>
2023-03-02 17:03:33 +0200

    betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,10 @@ void reverse_array(int *a, int n)
 {
 int temp, i;
 
-for (i = 0; i < n/2; i++)
+for (i = 0; i < n / 2; i++)
 {
 temp = a[i];
-a[i] = a[n-1-i];
-a[n-1-i] = temp;
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
 }
 }
