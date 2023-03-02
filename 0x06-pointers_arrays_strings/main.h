e6c3889bd0c2a72d0b93d1e8afdb5471a63721fc
Richard Gwati <haggiog0844@gmail.com>
2023-03-02 14:54:48 +0200

    strcmp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ extern int _putchar(char c);
 extern char *_strcat(char *dest, char *src);
 extern char *_strncat(char *dest, char *src, int n);
 extern char *_strncpy(char *dest, char *src, int n);
+extern int _strcmp(char *s1, char *s2);
 
 
 #endif /* MAIN_H */
