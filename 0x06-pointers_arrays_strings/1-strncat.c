d442aae1c2c97b5cec7c4f84b51addc051ee5c3d
Richard Gwati <haggiog0844@gmail.com>
2023-03-02 13:59:12 +0200

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,5 @@
-
-
 /**
-* *_strncat -  concatenates two strings
+* _strncat -  concatenates two strings
 * @dest: This is pointer to the destination array
 * @src: This is the string to be appended
 * @n: bytes from src
@@ -11,12 +9,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 char *p = dest;
+int i = 0;
 
 while (*p != '\0')
 {
 p++;
 }
-int i = 0;
 while (i < n && *src != '\0')
 {
 *p = *src;
