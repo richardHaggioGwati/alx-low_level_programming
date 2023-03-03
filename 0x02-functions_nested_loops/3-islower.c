955419a2923b88e1a2db9f62901c404f6c304a8e
Richard Gwati <haggiog0844@gmail.com>
2023-03-03 17:31:34 +0200

    LOWERCASE

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,17 +3,16 @@
 /**
  * _islower - checks for lowercase character.
  * @c: receives character
- * return: 1 if c is lowercase : Returns 0 otherwise
+ * Return: 1 if c is lowercase : Returns 0 otherwise
  */
 int _islower(int c)
 {
-
 if (islower(c) == 0)
 {
-    return (0);
+return (0);
 }
 else
 {
-    return (1);
+return (1);
 }
 }
