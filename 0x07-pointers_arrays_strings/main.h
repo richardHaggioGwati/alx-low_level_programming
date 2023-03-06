e54bcddf7498c700ab72e4d0935012ac3b9c56f6
Richard Gwati <haggiog0844@gmail.com>
2023-03-06 18:59:49 +0200

    Chess is mental torture

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ extern char *_strchr(char *s, char c);
 extern unsigned int _strspn(char *s, char *accept);
 extern char *_strpbrk(char *s, char *accept);
 extern char *_strstr(char *haystack, char *needle);
+extern void print_chessboard(char (*a)[8]);
 
 #endif /* MAIN_H */
  
