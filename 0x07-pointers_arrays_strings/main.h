6801a02788bc1d30e3c51f30e36079e0a6c9dbf4
Richard Gwati <haggiog0844@gmail.com>
2023-03-06 18:19:58 +0200

    2. strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 extern int _putchar(char c);
 extern char *_memset(char *s, char b, unsigned int n);
 extern char *_memcpy(char *dest, char *src, unsigned int n);
+extern char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
  
