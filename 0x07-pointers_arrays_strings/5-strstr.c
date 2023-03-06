a1e65e81bc18d6de3cd5e2fa7497f91740a0b898
Richard Gwati <haggiog0844@gmail.com>
2023-03-06 18:51:54 +0200

    style

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ int i, j;
 
 for (i = 0; haystack[i] != '\0'; i++)
 {
-for (j = 0; needle[j] != '\0' && haystack[i + j] == needle[j]; j++)
+for (j = 0; needle[j] != '\0' && haystack[i + j] == needle[j]; j++);
 if (needle[j] == '\0')
 {
 return (&haystack[i]);
