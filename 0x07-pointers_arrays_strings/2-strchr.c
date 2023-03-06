9ef4fe3e217a57c7702ad054445cb38ebd5074ae
Richard Gwati <haggiog0844@gmail.com>
2023-03-06 18:22:32 +0200

    2. strchr betty

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,8 @@
 
 char *_strchr(char *s, char c)
 {
-while (*s != '\0') {
+while (*s != '\0')
+{
 if (*s == c)
 {
 return (s);
@@ -18,7 +19,7 @@ s++;
 }
 if (c == '\0')
 {
-return s;
+return (s);
 }
-return NULL;
+return (NULL);
 }
