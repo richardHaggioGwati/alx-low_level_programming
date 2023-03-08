7991b956fb833af35e2bb8f386320e9f440a0193
Richard Gwati <haggiog0844@gmail.com>
2023-03-08 18:19:29 +0200

    removed loop

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,26 +7,34 @@
  * Return: integer
  */
 
+int helper(int n, int i)
+{
+if (i * i == n)
+{
+return (i);
+}
+else if (i * i > n)
+{
+return (-1);
+}
+else
+{
+return (helper(n, i + 1));
+}
+}
+
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 {
-return -1;
+return (-1);
 }
-else if (n == 0 || n == 1)
+else if (n == 0)
 {
-return n;
+return (0);
 }
 else
 {
-int i;
-for (i = 1; i * i <= n; i++)
-{
-if (i * i == n)
-{
-return i;
-}
-}
-return -1;
+return (helper(n, 1));
 }
 }
