f76431c38211bdc12afec78622031357a1661c11
Richard Gwati <haggiog0844@gmail.com>
2023-03-10 20:26:16 +0200

    corrections

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - prints its name
@@ -11,7 +10,6 @@
 int main(int argc, char *argv[])
 {
 (void) argc;
-
 printf("%s\n", argv[0]);
 return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,6 +20,6 @@ return (1);
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
 
-printf("%d\n", num1 * num2);
+printf("%d\n", num1 *num2);
 return (0);
 }
