fb9c46478ff2db9af0487452e3b3e9572337eef6
Richard Gwati <haggiog0844@gmail.com>
2023-03-10 20:54:52 +0200

    helper function

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,29 +2,14 @@
 #include <stdlib.h>
 
 /**
- * main - prints its name
- * @argc: argument count
- * @argv: argument array
- * Return: it's name
+ * calculateCoins - calculates the minimum number of coins
+ * @cents: the amount in cents
+ * Return: number of coins
  */
-
-int main(int argc, char *argv[])
+int calculateCoins(int cents)
 {
-int cents, coins;
+int coins = 0;
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-cents = atoi(argv[1]);
-
-if (cents < 0)
-{
-printf("0\n");
-return (0);
-}
-coins = 0;
 while (cents > 0)
 {
 if (cents >= 25)
@@ -49,6 +34,34 @@ cents -= 1;
 }
 coins++;
 }
+return (coins);
+}
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument array
+ * Return: it's name
+ */
+
+int main(int argc, char *argv[])
+{
+int cents, coins;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+coins = calculateCoins(cents);
+
 printf("%d\n", coins);
 return (0);
 }
