5e8726a0552410dd1d40be3af2a2a24a272d39ad
Richard Gwati <haggiog0844@gmail.com>
2023-03-10 15:12:49 +0200

    one liner

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Compile all .c files into object files
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-
-# Create a static library from the object files
-ar -rc liball.a *.o
-
-# Index the library
-ranlib liball.a
+gcc -Wall -Werror -Wextra -pedantic -c *.c && ar -rc liball.a *.o && ranlib liball.a && rm *.o
