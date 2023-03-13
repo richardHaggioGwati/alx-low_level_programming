12d711d63ce58d16b5870f400b1c5148953d7ae7
Richard Gwati <haggiog0844@gmail.com>
2023-03-13 12:33:21 +0200

    3-alloc_grid.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,5 +4,6 @@
 extern int _putchar(char c);
 extern char *create_array(unsigned int size, char c);
 extern char *str_concat(char *s1, char *s2);
+extern int **alloc_grid(int width, int height);
 
 #endif /* MAIN_H */
