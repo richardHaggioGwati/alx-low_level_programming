58da4aa493638593beecb28aced44572d04379c4
Richard Gwati <haggiog0844@gmail.com>
2023-03-13 12:21:44 +0200

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer 
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+char *concatenated;
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+concatenated = malloc(strlen(s1) + strlen(s2) + 1);
+if (concatenated == NULL)
+{
+return (NULL);
+}
+strcpy(concatenated, s1);
+strcat(concatenated, s2);
+
+return (concatenated);
+}
