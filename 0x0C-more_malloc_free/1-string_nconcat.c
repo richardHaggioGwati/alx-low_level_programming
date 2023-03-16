b24a0360073c915071c99fb38dca300389faf38d
Richard Gwati <haggiog0844@gmail.com>
2023-03-16 13:04:15 +0200

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,54 +3,60 @@
 #include <string.h>
 
 /**
+ * string_len - the length of a string
+ * @str: The string to get the length of.
+ * Return: The length of the string.
+ */
+unsigned int string_len(char *str) {
+if (str == NULL)
+{
+return (0);
+}
+else
+{
+return strlen(str);
+}
+}
+
+/**
+ * string_copy - Copies a string into a newly allocated memory block, 
+ * @str: The string to copy.
+ * Return: A pointer to a newly allocated string
+ */
+char *string_copy(char *str) {
+unsigned int len = string_len(str);
+char *result = malloc(len + 1);
+
+if (result == NULL) {
+return NULL;
+}
+else
+{
+strcpy(result, str);
+return (result);
+}
+}
+
+/**
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
  * @n: unknown number of bytes
  * Return: the new concatenated string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int s1_len, s2_len, i, j;
+unsigned int s1_len = string_len(s1);
+char *result = malloc(s1_len + n + 1);
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
-
-    if (n >= s2_len)
-    {
-        n = s2_len;
-    }
-
-    result = malloc(s1_len + n + 1);
-
-    if (result == NULL)
-    {
-        return NULL;
-    }
-
-    for (i = 0; i < s1_len; i++)
-    {
-        result[i] = s1[i];
-    }
-
-    for (j = 0; j < n; j++)
-    {
-        result[i + j] = s2[j];
-    }
-
-    result[i + j] = '\0';
-
-    return result;
+if (result == NULL)
+{
+return (NULL);
+}
+else
+{
+strcpy(result, s1);
+strncat(result, s2, n);
+return (result);
+}
 }
