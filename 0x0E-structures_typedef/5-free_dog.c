d976fdce5bac6889e96ebbc4dd124ac1fa0c9783
Richard Gwati <haggio0844@gmail.com>
2023-03-20 19:00:14 +0200

    betty

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,9 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * free_dog - free the dog stored in memory
+ * @d: - pointer to the dog
+ * 
+ * Returns: void 
+*/
 void free_dog(dog_t *d)
 {
-    free(d->name);
-    free(d->owner);
-    free(d);
+free(d->name);
+free(d->owner);
+free(d);
 }
