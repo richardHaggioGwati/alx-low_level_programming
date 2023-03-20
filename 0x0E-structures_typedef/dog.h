18fb5ecd1c9380265bea834f7eb763ba8722f99e
Richard Gwati <haggio0844@gmail.com>
2023-03-20 18:53:30 +0200

    free dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,6 @@ char *owner;
 extern void init_dog(struct dog *d, char *name, float age, char *owner);
 extern void print_dog(struct dog *d);
 extern dog_t *new_dog(char *name, float age, char *owner);
+extern void free_dog(dog_t *d);
 
 #endif
