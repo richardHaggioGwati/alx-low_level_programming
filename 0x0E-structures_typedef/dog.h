6dc8421ad4278aa7d7d2428b33cffebcf81272f6
Richard Gwati <haggio0844@gmail.com>
2023-03-20 13:31:08 +0200

    dog.h description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+/**
+ * dog - structure that defines the shape of a dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 char *name;
