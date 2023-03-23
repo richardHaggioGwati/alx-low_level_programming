e585ae1e9e053dbf7b19cf520846aab7b308603b
Richard Gwati <haggiog0844@gmail.com>
2023-03-23 19:16:50 +0200

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,42 +10,52 @@
  */
 void print_all(const char *const format, ...)
 {
-int i = 0;
-char c;
-char *s;
-float f;
-va_list args;
-va_start(args, format);
+    char c_val;
+    const char *f = format;
+    int i_val;
+    float f_val;
+    char *s_val;
+    va_list arg_list;
+    va_start(arg_list, format);
 
-while (format[i])
-{
-switch (format[i])
-{
-case 'i':
-printf("%d ", va_arg(args, int));
-break;
-case 'c':
-c = va_arg(args, int);
-printf("%c ", c);
-break;
-case 'f':
-f = va_arg(args, double);
-printf("%f ", f);
-break;
-case 's':
-s = va_arg(args, char *);
-if (s == NULL)
-{
-printf("(nil)");
-}
-else
-{
-printf("%s ", s);
-}
-break;
-}
-i++;
-}
-printf("\n");
-va_end(args);
+
+    while (*f)
+    {
+        if (*f == 'c')
+        {
+            c_val = (char)va_arg(arg_list, int);
+            printf("%c", c_val);
+        }
+        else if (*f == 'i')
+        {
+            i_val = va_arg(arg_list, int);
+            printf("%d", i_val);
+        }
+        else if (*f == 'f')
+        {
+            f_val = (float)va_arg(arg_list, double);
+            printf("%f", f_val);
+        }
+        else if (*f == 's')
+        {
+            s_val = va_arg(arg_list, char *);
+            if (s_val == NULL)
+            {
+                printf("(nil)");
+            }
+            else
+            {
+                printf("%s", s_val);
+            }
+        }
+
+        f++;
+        if (*format)
+        {
+            printf(", ");
+        }
+    }
+
+    va_end(arg_list);
+    printf("\n");
 }
